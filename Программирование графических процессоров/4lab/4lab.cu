b2cb8b248824ac3cf0e3e2e0818c8ef9c4ddfc74
Pavel Gamov <pagamov@gmail.com>
2021-11-02 20:24:13 +0300

    pgp 4lab
    stable version no optimising 5-6 test time limit

---
+++ Программирование графических процессоров/4lab/4lab.cu
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <thrust/device_vector.h>
+#include <thrust/extrema.h>
+#include <thrust/count.h>
+
+using namespace std;
+
+#define CSC(call)  													\
+do {																\
+	cudaError_t res = call;											\
+	if (res != cudaSuccess) {										\
+		fprintf(stderr, "ERROR in %s:%d. Message: %s\n",			\
+				__FILE__, __LINE__, cudaGetErrorString(res));		\
+		exit(0);													\
+	}																\
+} while(0)
+
+__global__ void findMax(double * A, int * SWP, int i, int n) {
+    double pivotValue = 0;
+    int pivot = -1;
+    for(int row = i; row < n; row++) {
+        if(abs(A[n * i + row]) > pivotValue) {
+            pivotValue = abs(A[n * i + row]);
+            pivot = row;
+        }
+    }
+    SWP[i] = pivot;
+}
+
+__global__ void LUP(double * A, int * SWP, int i, int n) {
+    int pivot = SWP[i];
+    double piv;
+    for (int sw = 0; sw < n; sw++) {
+       piv = A[pivot + n * sw];
+       A[pivot + n * sw] = A[i + n * sw];
+       A[i + n * sw] = piv;
+    }
+    for(int j = i+1; j < n; j++) {
+       A[j + n * i] /= A[i + n * i];
+       for(int k = i+1; k < n; k++) 
+           A[j + n * k] -= A[j + n * i] * A[i + n * k];
+    }
+}
+
+int main() {
+    int n;
+    cin >> n;
+    double * A = (double *)malloc(sizeof(double) * n * n);
+    
+    for (int y = 0; y < n; y++)
+        for (int x = 0; x < n; x++)
+            cin >> A[x * n  + y];
+            
+    double * A_DEV;
+    CSC(cudaMalloc(&A_DEV, sizeof(double) * n * n));
+    CSC(cudaMemcpy(A_DEV, A, sizeof(double) * n * n, cudaMemcpyHostToDevice));
+    
+    int * SWP = (int *)malloc(sizeof(int) * n);
+    int * SWP_DEV;
+    CSC(cudaMalloc(&SWP_DEV, sizeof(int) * n));
+    
+    for(int i = 0; i < n; i++) {
+        findMax <<<1,1>>> (A_DEV, SWP_DEV, i, n);
+        LUP <<<1,1>>> (A_DEV, SWP_DEV, i, n);
+    }
+                                      
+    CSC(cudaMemcpy(A, A_DEV, sizeof(double) * n * n, cudaMemcpyDeviceToHost));
+    CSC(cudaMemcpy(SWP, SWP_DEV, sizeof(int) * n, cudaMemcpyDeviceToHost));
+    
+    for (int y = 0; y < n; y++) {
+        for (int x = 0; x < n; x++)
+            printf("%lf ", A[x * n + y]);
+        printf("\n");
+    }
+    for (int i = 0; i < n; i++)
+        printf("%d ", SWP[i]);
+    printf("\n");
+    
+    CSC(cudaFree(A_DEV));
+    CSC(cudaFree(SWP_DEV));
+    free(A);
+    free(SWP);
+    return 0;
+}
