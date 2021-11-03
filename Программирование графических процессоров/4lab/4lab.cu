17e67e11dec85679757e2a8b3a3b5525646e9ad9
Pavel Gamov <pagamov@gmail.com>
2021-11-04 02:08:40 +0300

    pgp 4lab
    using trust now
    5t time error

--- Программирование графических процессоров/4lab/4lab.cu
+++ Программирование графических процессоров/4lab/4lab.cu
@@ -18,26 +18,43 @@ do {																\
 	}																\
 } while(0)
 
-__global__ void findMax(double * A, int * SWP, int i, int n) {
-    double pivotValue = 0;
-    int pivot = -1;
-    for(int row = i; row < n; row++) {
-        if(abs(A[n * i + row]) > pivotValue) {
-            pivotValue = abs(A[n * i + row]);
-            pivot = row;
-        }
-    }
-    SWP[i] = pivot;
-}
+// __global__ void findMax(double * A, int * SWP, int i, int n) {
+//     double pivotValue = 0;
+//     int pivot = -1;
+//     for(int row = i; row < n; row++) {
+//         if(abs(A[n * i + row]) > pivotValue) {
+//             pivotValue = abs(A[n * i + row]);
+//             pivot = row;
+//         }
+//     }
+//     SWP[i] = pivot;
+// }
 
-__global__ void LUP(double * A, int * SWP, int i, int n) {
-    int pivot = SWP[i];
+struct comparator {												
+	__host__ __device__ bool operator()(double a, double b) {		// Функция которая сравнивает объекты на "<"
+		return abs(a) < abs(b);										// operator() - переопределение оператора "()" для экземпляра этой структуры
+	}
+};
+
+__global__ void LUP(double * A, int * SWP, int i, int n, int newidx) {
+    // int pivot = SWP[i];
+	
+	// int idx = x = blockDim.x * blockIdx.x + threadIdx.x;
+	// int shift = blockDim.x * gridDim.x;
+	
+	int pivot = newidx;
     double piv;
+	// __shared__ double piv[blockDim.x];
+	
     for (int sw = 0; sw < n; sw++) {
        piv = A[pivot + n * sw];
        A[pivot + n * sw] = A[i + n * sw];
        A[i + n * sw] = piv;
     }
+	// if (idx < n) {}
+	// piv = A[pivot + n * sw];
+	
+	// __syncthreads();
     for(int j = i+1; j < n; j++) {
        A[j + n * i] /= A[i + n * i];
        for(int k = i+1; k < n; k++) 
@@ -47,12 +64,13 @@ __global__ void LUP(double * A, int * SWP, int i, int n) {
 
 int main() {
     int n;
+	comparator comp;
     cin >> n;
     double * A = (double *)malloc(sizeof(double) * n * n);
     
     for (int y = 0; y < n; y++)
         for (int x = 0; x < n; x++)
-            cin >> A[x * n  + y];
+            cin >> A[x * n + y];
             
     double * A_DEV;
     CSC(cudaMalloc(&A_DEV, sizeof(double) * n * n));
@@ -62,9 +80,21 @@ int main() {
     int * SWP_DEV;
     CSC(cudaMalloc(&SWP_DEV, sizeof(int) * n));
     
+	int newidx;
+	
+	int * newidxarr = (int *)malloc(sizeof(int) * n);
+	
     for(int i = 0; i < n; i++) {
         findMax <<<1,1>>> (A_DEV, SWP_DEV, i, n);
-        LUP <<<1,1>>> (A_DEV, SWP_DEV, i, n);
+		
+		thrust::device_ptr<double> d_ptr = thrust::device_pointer_cast(A_DEV) + (i * n + i);
+		thrust::device_ptr<double> max = thrust::max_element(d_ptr, d_ptr + (n - i), comp);
+		newidx = max - d_ptr + i;
+		// cout << newidx << " " << *(max) << endl;
+		newidxarr[i] = newidx;
+		
+        LUP <<<1,1>>> (A_DEV, SWP_DEV, i, n, newidx);
+		// LUP <<<2,2>>> (A_DEV, SWP_DEV, i, n, newidx);
     }
                                       
     CSC(cudaMemcpy(A, A_DEV, sizeof(double) * n * n, cudaMemcpyDeviceToHost));
@@ -75,9 +105,17 @@ int main() {
             printf("%lf ", A[x * n + y]);
         printf("\n");
     }
-    for (int i = 0; i < n; i++)
-        printf("%d ", SWP[i]);
+    for (int i = 0; i < n; i++) {
+		printf("%d ", newidxarr[i]);
+		// if (SWP[i] != newidxarr[i]) {
+		// 	fprintf(stderr, "%d,%d", SWP[i], newidxarr[i]);
+		// }
+	}
+		
+        
     printf("\n");
+	
+	free(newidxarr);
     
     CSC(cudaFree(A_DEV));
     CSC(cudaFree(SWP_DEV));
