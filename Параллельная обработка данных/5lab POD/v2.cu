87b47023976afcdff7999db042d8794989765c9f
Pavel <52429594+pagamov@users.noreply.github.com>
2022-03-09 17:20:52 +0300

    pc

--- Параллельная обработка данных/5lab POD/v2.cu
+++ Параллельная обработка данных/5lab POD/v2.cu
@@ -4,13 +4,13 @@
 #include <sstream>
 #include <string>
 #include <vector>
-#include <algorithm>
-#include <climits>
+// #include <algorithm>
+// #include <climits>
 #include <thrust/swap.h>
-#include <thrust/extrema.h>
-#include <thrust/functional.h>
-#include <thrust/host_vector.h>
-#include <thrust/device_vector.h>
+// #include <thrust/extrema.h>
+// #include <thrust/functional.h>
+// #include <thrust/host_vector.h>
+// #include <thrust/device_vector.h>
 
 using namespace std;
 
@@ -30,63 +30,49 @@ do {                                                                \
 __global__ void oddEvenSortingStep(int * A, int i, int n, int batch) {
     int idx = blockDim.x * blockIdx.x + threadIdx.x;
     int shift = blockDim.x * gridDim.x;
-    // int piv;
     for (int start = idx * batch; start < n; start += shift * batch) {
         for (int j = start + (i % 2); j + 1 < min(start + batch, n); j += 2) {
             if (A[j] > A[j + 1]) {
                 thrust::swap(A[j], A[j + 1]);
-                // piv = A[j];
-                // A[j] = A[j + 1];
-                // A[j + 1] = piv;
             }
         }
     }
 }
 
 __device__ void swap_step(int* nums, int* tmp, int size, int start, int stop, int step, int i) {
-	// Using shared memory to store blocks and sort them
-	__shared__ int sh_array[BLOCK_SIZE];
+	__shared__ int sh[BLOCK_SIZE];
 
-	// Step for bitonic merge inside merging
 	for (int shift = start; shift < stop; shift += step) {
-		// New start pointer
 		tmp = nums + shift;
 
-		// Right side
 		if (i >= BLOCK_SIZE / 2)
-			sh_array[i] = tmp[BLOCK_SIZE * 3 / 2 - 1 - i];
+			sh[i] = tmp[BLOCK_SIZE * 3 / 2 - 1 - i];
 		else
-			sh_array[i] = tmp[i];
-
+			sh[i] = tmp[i];
 		__syncthreads();
 
-		// From half
 		for (int j = BLOCK_SIZE / 2; j > 0; j /= 2) {
 			unsigned int XOR = i ^ j;
 			// The threads with the lowest ids sort the array
 			if (XOR > i) {
 				if ((i & BLOCK_SIZE) != 0) {
-					// Step descending, swap(i, XOR)
-					if (sh_array[i] < sh_array[XOR])
-						thrust::swap(sh_array[i], sh_array[XOR]);
+					if (sh[i] < sh[XOR])
+						thrust::swap(sh[i], sh[XOR]);
 				} else {
-					// Step ascending, swap(i, XOR)
-					if (sh_array[i] > sh_array[XOR])
-						thrust::swap(sh_array[i], sh_array[XOR]);
+					if (sh[i] > sh[XOR])
+						thrust::swap(sh[i], sh[XOR]);
 				}
 			}
-
 			__syncthreads();
 		}
 
-		// Back from shared to temporary
-		tmp[i] = sh_array[i];
+		tmp[i] = sh[i];
 	}
 }
 
-__global__ void kernel_bitonic_merge_step(int* nums, int size, bool is_odd, bool flag) {
+__global__ void kernel_bitonic_merge_step(int * nums, int size, bool is_odd) {
 	// Temporary array for splitting into blocks
-	int* tmp = nums;
+	int * tmp = nums;
 
 	// Every thread gets exactly one value in the unsorted array
 	unsigned int i = threadIdx.x;
@@ -99,52 +85,6 @@ __global__ void kernel_bitonic_merge_step(int* nums, int size, bool is_odd, bool
 	} else { // For even step
 		swap_step(nums, tmp, size, id_block * BLOCK_SIZE, size, offset * BLOCK_SIZE, i);
 	}
-}
-
-__global__ void bitonic_sort_step(int *nums, int j, int k, int size) {
-	// Using shared memory to store blocks and sort them
-	__shared__ int sh_array[BLOCK_SIZE];
-
-	// Temporary array for splitting into blocks
-	int * tmp = nums;
-
-	// Every thread gets exactly one value in the unsorted array
-	unsigned int i = threadIdx.x;
-	int id_block = blockIdx.x;
-	int offset = gridDim.x;
-
-	// Step for bitonic sort
-	for (int shift = id_block * BLOCK_SIZE; shift < size; shift += offset * BLOCK_SIZE) {
-			// New start pointer
-			tmp = nums + shift;
-
-			// Store in shared memory
-			sh_array[i] = tmp[i];
-
-			__syncthreads();
-
-			// From half
-			for (j = k / 2; j > 0; j /= 2) {
-				unsigned int XOR = i ^ j;
-				// The threads with the lowest ids sort the array
-				if (XOR > i) {
-					if ((i & k) != 0) {
-						// Step descending, swap(i, XOR)
-						if (sh_array[i] < sh_array[XOR])
-							thrust::swap(sh_array[i], sh_array[XOR]);
-					} else {
-						// Step ascending, swap(i, XOR)
-						if (sh_array[i] > sh_array[XOR])
-							thrust::swap(sh_array[i], sh_array[XOR]);
-					}
-				}
-
-				__syncthreads();
-			}
-
-			// Back from shared to temporary
-			tmp[i] = sh_array[i];
-		}
 }
 
 int main() {
@@ -172,33 +112,13 @@ int main() {
 
 	CSC(cudaMemcpy(dev_data, data, sizeof(int) * upd_n, cudaMemcpyHostToDevice));
 
-	// Pre sort of all blocks by bitonic sort
-	// Main step
-	// for (int k = 2; k <= upd_n; k *= 2) {
-	// 	if (k > BLOCK_SIZE)
-	// 		break;
-	// 	// Merge and split step
-	// 	for (int j = k / 2; j > 0; j /= 2) {
-	// 		bitonic_sort_step<<<NUM_BLOCKS, BLOCK_SIZE>>>(dev_data, j, k, upd_n);
-	// 		CSC(cudaGetLastError());
-	// 	}
-	// }
-
-	for (int i = 0; i < BLOCK_SIZE; i++) {
+	for (int i = 0; i < BLOCK_SIZE; i++)
         oddEvenSortingStep <<<NUM_BLOCKS,BLOCK_SIZE>>> (dev_data, i, n, BLOCK_SIZE);
-    }
 
-	/*
-	Implementation of odd-even sort
-	Sort of buckets with bitonic merge inside
-	| 1 3 5 7 | 2 4 6 8 | -> | 1 2 3 4 5 6 7 8| (n == 8)
+	for (int i = 0; i < 2 * (upd_n / BLOCK_SIZE); i++)
+		kernel_bitonic_merge_step<<<NUM_BLOCKS, BLOCK_SIZE>>>(dev_data, upd_n, (bool)(i % 2));
 
-	Including 2 steps merge + splitting
-	*/
-	for (int i = 0; i < 2 * (upd_n / BLOCK_SIZE); ++i) {
-		kernel_bitonic_merge_step<<<NUM_BLOCKS, BLOCK_SIZE>>>(dev_data, upd_n, (bool)(i % 2), true);
-		CSC(cudaGetLastError());
-	}
+	CSC(cudaGetLastError());
 
 	CSC(cudaMemcpy(data, dev_data, sizeof(int) * upd_n, cudaMemcpyDeviceToHost));
 
